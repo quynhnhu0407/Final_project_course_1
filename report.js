fc318a17f00ab3377879323d70f97c6ec25d0002
Quynh Nhu <quynhnhu0407@example.com>
2026-09-14 20:55:03 +0700

    Fix API_BASE to use dynamic origin for ngrok compatibility

--- report.js
+++ report.js
@@ -1,5 +1,5 @@
-// API Base URL
-const API_BASE = 'http://localhost:5000';
+// API Base URL - use relative path to work with both localhost and ngrok
+const API_BASE = window.location.origin;
 
 // Store chart instances
 let chartInstances = {};
